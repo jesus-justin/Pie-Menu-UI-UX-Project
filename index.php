ade44277c2441b76c3d907db3094f572314449dd
jesus-justin <mjesusjustin@gmail.com>
2026-01-11 15:14:03 +0800

    feat: Add meta description for SEO

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ $menuItems = [
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Kando-inspired radial pie menu built with PHP" />
     <title>Kando-inspired Pie Menu (PHP)</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
