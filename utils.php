11a54289a6567f51f1295abf3838f1e5a3dffd54
jesus-justin <mjesusjustin@gmail.com>
2026-01-20 20:49:36 +0800

    Add CSRF helper functions to utils

--- utils.php
+++ utils.php
@@ -78,3 +78,23 @@ function formatTimestamp(?string $timestamp): string {
     $date = new DateTime($timestamp);
     return $date->format('M j, Y g:i A');
 }
+
+/**
+ * CSRF helpers
+ */
+function ensureCsrfToken(): string {
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function isValidCsrf(?string $token): bool {
+    if (session_status() !== PHP_SESSION_ACTIVE) {
+        session_start();
+    }
+    return is_string($token) && hash_equals($_SESSION['csrf_token'] ?? '', $token);
+}
