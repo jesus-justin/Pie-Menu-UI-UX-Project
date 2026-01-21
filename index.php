f68b19353bb7083f22287d18d0afd1c2b7b9c221
jesus-justin <mjesusjustin@gmail.com>
2026-01-21 19:36:28 +0800

    Add SEO meta tags to landing page

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Welcome to our innovative platform with intuitive navigation" />
+    <meta name="keywords" content="pie menu, radial menu, navigation, interactive UI, UX design" />
+    <meta name="author" content="Jesus Justin" />
     <meta name="theme-color" content="#0a0f1f" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
