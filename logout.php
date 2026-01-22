3ffcf10cb4ca50be79dffdcc933da8065c8833e6
jesus-justin <mjesusjustin@gmail.com>
2026-01-22 20:05:39 +0800

    Clear remember-me cookie on logout

--- logout.php
+++ logout.php
@@ -3,10 +3,18 @@ session_start();
 
 // Clear all session data and destroy the session
 $_SESSION = [];
+
+// Delete session cookie
 if (ini_get('session.use_cookies')) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
+
+// Delete remember-me cookie if exists
+if (isset($_COOKIE['remember_token'])) {
+    setcookie('remember_token', '', time() - 42000, '/', '', isset($_SERVER['HTTPS']), true);
+}
+
 session_destroy();
 
 // Redirect to login with a logged_out flag
